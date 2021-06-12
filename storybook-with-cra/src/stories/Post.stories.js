44fa242c705c2869ca681cccfe0eb0847825661c
31 <monsterfairy2@gmail.com>
2021-06-12 14:52:26 +0900

    예제 분리

--- storybook-with-cra/src/stories/Post.stories.js
+++ storybook-with-cra/src/stories/Post.stories.js
@@ -10,11 +10,18 @@ export default {
 
 const Template = (args) => <Post {...args} />;
 
-export const Basic = Template.bind({});
-Basic.args = {
+export const Default = Template.bind({});
+
+
+export const Title = Template.bind({});
+Title.args = {
+  title:'제목입니다.',
+};
+
+export const TitleAndContent = Template.bind({});
+TitleAndContent.args = {
   title:'제목입니다.',
   refList:['출처1', '출처2'],
   children:<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 };
-
 
