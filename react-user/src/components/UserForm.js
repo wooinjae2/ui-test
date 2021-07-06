969859b9e75343a288d7f074b792f8fd933d3df8
31 <monsterfairy2@gmail.com>
2021-07-06 20:52:52 +0900

    add reusable button component

--- react-user/src/components/UserForm.js
+++ react-user/src/components/UserForm.js
@@ -1,3 +1,4 @@
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from './UserForm.module.css'
 
@@ -18,7 +19,7 @@ const UserForm = (props) => {
           Age
         </label>
         <input id="age" type="text" />
-        <button type="submit">Add User </button>
+        <Button type="submit">Add User </Button>
       </form>
     </Card>
   );
