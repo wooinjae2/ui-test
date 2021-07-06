e142269716c82e6e76e1b16fa00745955728e2c5
31 <monsterfairy2@gmail.com>
2021-07-06 20:45:19 +0900

    Reusable Cart컴포넌트

--- react-user/src/components/UserForm.js
+++ react-user/src/components/UserForm.js
@@ -1,40 +1,27 @@
-import {useState} from 'react';
-import Modal from './Modal';
+import Card from "../UI/Card";
+import classes from './UserForm.module.css'
 
-function UserForm (props) {
+const UserForm = (props) => {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+  };
 
-    const [userName, setUserName] = useState('');
-    const [userAge, setUserAge] = useState('');
-    const [modalOpen, setModalOpen] = useState(false);
-    
-    const saveHandler = () =>{
-        if(validCheck()){
-            props.saveUserInfo({name:userName, age:userAge});
-        }else{
-            setModalOpen(true);
-        }
-        
-    }
+  console.log(classes.input);
 
-    const validCheck = () =>{
-        if(userName === '' || userAge === ''){
-            return false;
-        }
-        return true;
-    }
-
-    const onClickConfirm = () => {
-        setModalOpen(false);
-    }
-
-    return (
-        <div>
-            <Modal modalOpen={modalOpen} onClickConfirm={onClickConfirm}/>
-            <input value={userName} onChange={e =>{setUserName(e.target.value)}} />
-            <input value={userAge} onChange={e =>{setUserAge(e.target.value)}} />
-            <button onClick={saveHandler}>Save</button>
-        </div>
-    )
-}
+  return (
+      
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">UserName</label>
+        <input id="username" type="text" />
+        <label htmlFor="age" type="number">
+          Age
+        </label>
+        <input id="age" type="text" />
+        <button type="submit">Add User </button>
+      </form>
+    </Card>
+  );
+};
 
 export default UserForm;
