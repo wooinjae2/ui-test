95e703a236c8a5d4fbadf270d7e9515bab8cf1ea
31 <monsterfairy2@gmail.com>
2021-07-11 16:06:37 +0900

    useEffect 최적화를 위한 세부 디펜던시 참조 적용

--- 01-starting-project2/src/components/Home/Home.js
+++ 01-starting-project2/src/components/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '../UI/Button/Button';
+
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
@@ -6,7 +8,9 @@ import classes from './Home.module.css';
 const Home = (props) => {
   return (
     <Card className={classes.home}>
+
       <h1>Welcome back!</h1>
+      <Button onClick={props.onLogout}>Logout</Button>
     </Card>
   );
 };
--- 01-starting-project2/src/components/Login/Login.js
+++ 01-starting-project2/src/components/Login/Login.js
@@ -10,11 +10,13 @@ const emailReducer = (state, action) => {
     return {value: action.val, isValid: action.val.includes('@')};
   }
   if(action.type === 'USER_BLUR'){
-    debugger;
+  
     return {value: state.value, isValid: state.value.includes('@')};
   }
   return { value: '', isValid: false };
 }; //reducer fn 이 컴포넌트 밖에 있다.
+
+
 
 const passwordReducer = (state, action) => {
   if(action.type === 'USER_INPUT'){
@@ -29,8 +31,8 @@ const passwordReducer = (state, action) => {
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
   const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: false}); 
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value: '', isValid: false}); 
@@ -46,29 +48,38 @@ const Login = (props) => {
   //3. reducerFn : (prevState, action) => {return newState; } 자동으로 트리거 되는 함수(dispatchFn를 통해)로 최신 상태의 스테이트와 snapshot을 받아서 new state를 리턴할것이다.
   //4. 
   //5. initFn : initialValue를 초기화 할수있다.
+  const {isValid : emailIsValid} = emailState; //useEffect 최적화 방법
+  const {isValid : passwordIsValid} = passwordState;
 
 
-  // useEffect(()=>{
-  //   console.log('chceck validity');
-  //   const identifier = setTimeout( () =>{
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6)
-  //   }, 1000)
-  //   return () =>{
-  //     console.log('clean up');
+  useEffect(()=>{
+    console.log('chceck validity');
+    // const identifier = setTimeout( () =>{
+      setFormIsValid(
+        emailIsValid && passwordIsValid)
+    // }, 500)
+    return () =>{
+      console.log('clean up');
+    }
       
-  // }, [enteredPassword , enteredEmail]);
+  }, [emailIsValid , passwordIsValid]); //useEffect 최적화 방법
+  //emailState Object전체를 디펜던시 목록에 넣지 않고 특정한 프로퍼티만 넣어서 useEffect를 최적화한다.
+  //디펜던시로 [someObject.someProperty]); 이런 형태도 가능
+  //아래와 같은 형태는 피하는게 좋다.
+  //}, [someObject]);
+
+
 
   
   const emailChangeHandler = (event) => {
     console.log()
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
-    setFormIsValid(emailState.isValid && passwordState.isValid);
+    // setFormIsValid(emailState.isValid && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({type: 'USER_INPUT', val: event.target.value});
-    setFormIsValid(emailState.isValid && passwordState.isValid);
+    // setFormIsValid(emailState.isValid && passwordState.isValid);
     // setFormIsValid(enteredEmail.includes('@') && passwordState.value.trim().length > 6)
   };
 
