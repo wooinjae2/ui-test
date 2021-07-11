f0675b4fc31de0fac8f3bbe4330bd94cb30ab994
31 <monsterfairy2@gmail.com>
2021-07-11 16:35:37 +0900

    context.consumer -> useContext hook 사용으로 변경

--- 01-starting-project2/src/components/MainHeader/Navigation.js
+++ 01-starting-project2/src/components/MainHeader/Navigation.js
@@ -1,35 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext); //useContext Hook을 이용한 context접근 더 깔끔해 보인다.
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        //컨슈머는 차일드를 함수의 리턴으로 받는다.
-        
-        return <nav className={classes.nav}>
-          <ul>
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Users</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Admin</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <button onClick={props.onLogout}>Logout</button>
-              </li>
-            )}
-          </ul>
-        </nav>;
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
