46868f0c29f5bb45d669833f188dc4842fbce659
31 <monsterfairy2@gmail.com>
2021-07-11 16:32:24 +0900

    context 사용 1. Consumer를 이용한 context 접근

--- 01-starting-project2/src/App.js
+++ 01-starting-project2/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
 
@@ -29,15 +30,23 @@ function App() {
     localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false);
   };
-
+//1. auth context만듬
+//2. authcontext를 import해서 이 컨텍스트 데이터를 사용할 모든 데이터를 감싸줌
+//3. 2가지로 context 데이터 접근 가능 1. provider의 데이터를 리슨할수 있음 2. context hook consumer 제거
+//
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+      <AuthContext.Provider 
+        value={{
+          isLoggedIn: isLoggedIn// provider에 default value를 넣어준다.
+        }}
+      >
+        <MainHeader onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
+
   );
 }
 
