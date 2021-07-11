04d8b67561d1bc372379f8d3cae45283a691ae7b
31 <monsterfairy2@gmail.com>
2021-07-11 21:12:57 +0900

    useImperativeHadnle & forwardRef

--- 01-starting-project2/src/components/Login/Login.js
+++ 01-starting-project2/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer, useContext } from 'react';
+import React, { useEffect, useState, useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -99,17 +99,25 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log('asdsad');
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if(formIsValid){
+      authCtx.onLogin(emailState.value, passwordState.value);
+    }else if(!emailIsValid){
+      emailInputRef.current.focus();
+    }else{
+      passwordInputRef.current.focus();
+    }
   };
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-      <Input id="email" label="E-mail" type="email" isValid={emailIsValid} value={emailState.value} onChange={emailChangeHandler} onBlur={validateEmailHandler}/>
-      <Input id="password" label="Password" type="password" isValid={passwordIsValid} value={passwordState.value} onChange={passwordChangeHandler} onBlur={validatePasswordHandler}/>
+      <Input ref={emailInputRef} id="email" label="E-mail" type="email" isValid={emailIsValid} value={emailState.value} onChange={emailChangeHandler} onBlur={validateEmailHandler}/>
+      <Input ref={passwordInputRef} id="password" label="Password" type="password" isValid={passwordIsValid} value={passwordState.value} onChange={passwordChangeHandler} onBlur={validatePasswordHandler}/>
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} >
             Login
           </Button>
         </div>
--- 01-starting-project2/src/components/UI/Input/Input.js
+++ 01-starting-project2/src/components/UI/Input/Input.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from 'react';
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+   const inputRef = useRef();
+
+   const activate = () => {
+       inputRef.current.focus();
+   }
+
+   useImperativeHandle(ref, ()=>{
+       return {
+        activate: activate
+       }
+   });
   return <div
   className={`${classes.control} ${
     props.isValid === false ? classes.invalid : ''
@@ -9,6 +20,7 @@ const Input = (props) => {
 >
   <label htmlFor={props.id}>{props.label}</label>
   <input
+    ref={inputRef}
     type={props.type}
     id={props.id}
     value={props.value}
@@ -18,6 +30,6 @@ const Input = (props) => {
 </div>
  
 
-}
+})
 
 export default Input;
