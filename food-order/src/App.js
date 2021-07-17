04603e1dcdabae70f27fe9c9581bfd35f8d8e7b2
31 <monsterfairy2@gmail.com>
2021-07-17 15:35:22 +0900

    modal 기능 추가

--- food-order/src/App.js
+++ food-order/src/App.js
@@ -1,12 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalClickHandler = () => {
+    setModalOpen((prevState) => !prevState);
+  }
+
+  const modalCloseHandler = () => {
+    setModalOpen(false);
+  }
+
   return (
     <Fragment>
-      <Header/>
+      <Cart modalOpen={modalOpen} modalCloseHandler={modalCloseHandler}/>
+      <Header modalClickHandler={modalClickHandler}/>
+      <main>
       <Meals/>
+      </main>
     </Fragment>
   );
 }
