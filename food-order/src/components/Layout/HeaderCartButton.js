528e24af31337408ff0a781bab8706319ccf5426
31 <monsterfairy2@gmail.com>
2021-07-18 14:45:29 +0900

    header cart button css 이벤트 추가

--- food-order/src/components/Layout/HeaderCartButton.js
+++ food-order/src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,34 @@
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnHighlighted] = useState(false); 
 
   const cartCtx = useContext(CartContext);
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
       return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : '' }`;
+  const { items } = cartCtx;
+
+  useEffect(()=>{
+      if(cartCtx.items.length === 0){
+          return;
+      }
+      console.log('asdsadas');
+    setBtnHighlighted(true);
+    const timer = setTimeout(()=>{setBtnHighlighted(false)}, 300);
+    return ()=>{
+        clearTimeout(timer);
+    }
+  },[cartCtx.items.length, items])
 
   return (
-    <button className={classes.button} onClick={props.modalClickHandler}>
+    <button className={btnClasses} onClick={props.modalClickHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
