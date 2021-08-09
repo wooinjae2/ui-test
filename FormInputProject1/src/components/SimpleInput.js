d7cd83007b308be42acdbcaf8e73fb916a08d12c
31 <monsterfairy2@gmail.com>
2021-08-09 21:11:51 +0900

    input form refactoring

--- FormInputProject1/src/components/SimpleInput.js
+++ FormInputProject1/src/components/SimpleInput.js
@@ -1,75 +1,52 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
+  
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  const [stepSize, setStepSize] = useState(1);
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
   }
 
-  useEffect(()=>{
-    if(enteredNameIsValid){
-      console.log('Name Input is valid');
-    }
-  }, [enteredNameIsValid])
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  
 
   const formSubmissionHandler = e => {
     e.preventDefault();
     setEnteredNameTouched(true);
-    if(enteredName.trim() === ''){
-      setEnteredNameIsValid(false);
+
+    if(!enteredNameIsValid){
       return;
     }
-    setEnteredNameIsValid(true);
-    console.log(enteredName);
-    const enteredValue = nameInputRef.current.value;
-    console.log('enteredValue', enteredValue);
     // nameInputRef.current.value = '';  NOT IDEAL, DON'T MANIPULATE THE DOM
-    setEnteredName('')
+    setEnteredName('');
+    setEnteredNameTouched(false);
   }
 
-  const handleStepSizeHandler = () =>{
-    if(stepSize === 1){
-      setStepSize(5);
-    }else{
-      setStepSize(1);
-    }
-  }
 
   const nameInputBlurHandler = () => {
     setEnteredNameTouched(true);
-    if(enteredName.trim() !== ''){
-      setEnteredNameIsValid(true);
-      return;
-    }else{
-      setEnteredNameIsValid(false);
-    }
   }
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid' ;
+  const nameInputClasses = !nameInputIsInvalid ? 'form-control' : 'form-control invalid' ;
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} 
-        type='text' 
-        id='name' 
-        onChange={nameInputChangeHandler} value={enteredName}
-        onBlur={nameInputBlurHandler}/>
+        <input  
+          type='text' 
+          id='name' 
+          onChange={nameInputChangeHandler} 
+          value={enteredName}
+          onBlur={nameInputBlurHandler}
+        />
       </div>
-      {!enteredNameIsValid &&<p className="error-text">Name must not be empty</p>}
-      {
-        !nameInputIsInvalid && (
-          <p className="error-text">Name must not be empty</p>
-        )
-      }
+      {nameInputIsInvalid &&<p className="error-text">Name must not be empty</p>}
       
       <div className="form-actions">
         <button>Submit</button>
